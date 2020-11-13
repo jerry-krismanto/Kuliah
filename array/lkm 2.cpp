c56744fe5fe67cf36ee3da0d0d84ab1ad4d55499
Jerry Yan Krismanto <72871486+jerry-krismanto@users.noreply.github.com>
2020-11-13 10:26:30 +0700

    ralat lkm 2

--- array/lkm 2.cpp
+++ array/lkm 2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
 
-int mtx[3][3],mtxt[3][3];
+int mtx[3][3];
 //memasukkan unsur matriks
 for (int i=0;i<3;++i){
     for(int j=0;j<3;++j){
@@ -21,25 +21,16 @@ for (int i=0;i<3;++i){
 }
 //mengubah/menukar nilai baris dengan nilai kolom dan menampilkan hasil
 cout<<"berikut adalah hasil transposenya: "<<endl<<endl;
-mtxt[0][0]=mtx[0][0];
-mtxt[1][0]=mtx[0][1];
-mtxt[2][0]=mtx[0][2];
-mtxt[0][1]=mtx[1][0];
-mtxt[1][1]=mtx[1][1];
-mtxt[2][1]=mtx[1][2];
-mtxt[0][2]=mtx[2][0];
-mtxt[1][2]=mtx[2][1];
-mtxt[2][2]=mtx[2][2];
 for (int i=0;i<3;++i){
     for(int j=0;j<3;++j){
-            cout<<"  "<<mtxt[i][j]<<"  ";}
-            cout<<endl<<endl;
-}
+            cout<<"  "<<mtx[j][i]<<"  ";}
+            cout<<endl<<endl;}
+
 cout<<"berikut adalah hasil penjumlahan matriks asli dan transposenya: "<<endl<<endl;
 //menampilkan hasil penjumlahan matriks asli dan transposenya
 for (int i=0;i<3;++i){
     for(int j=0;j<3;++j){
-            cout<<"  "<<mtx[i][j]+mtxt[i][j]<<"  ";}
+            cout<<"  "<<mtx[i][j]+mtx[j][i]<<"  ";}
             cout<<endl<<endl;
 }
 }
